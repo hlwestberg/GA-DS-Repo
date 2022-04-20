32e799220b87e518a3b6ecd7d2e73a3d3418b803
Hannah <37636565+hlwestberg@users.noreply.github.com>
2022-04-19 20:53:18 -0600

    syncing latest classwork and capstone project progress

--- ClassMaterial/Class7_linear_regression_2/README.md
+++
@@ -1,5 +0,0 @@
-### Introduction to Regression Continued
-
-- Polynomial Regression 
-- Bias vs. Variance
-- Train/Test Split and Cross Validation
---
+++ capstone/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My final project for the GA A Intro to Data Science Course.',
+    author='Hannah Westberg',
+    license='MIT',
+)
